77b4cb65298d38683c847f260940a7f4d4d799d6
BenevolenceEM <maliksofia58@gmail.com>
2025-08-16 20:41:49 -0400

    Create Members.java

---
+++ classes/Members.java
@@ -0,0 +1,27 @@
+public class Members {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+
+    //Constructors
+    public Members() {
+        this.email = "";
+        this.password = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.phoneNo = "";
+    }
+
+    public Members(Members m) {
+        this.email = m.email;
+        this.password = m.password;
+        this.firstName = m.firstName;
+        this.lastName = m.lastName;
+        this.phoneNo = m.phoneNo;
+    }
+
+    //Get and set methods
+    public String 
+}
